9f2951cc22a5ec83393abd19f8279d7cc31e84bd
duttar18 <duttaraul@gmail.com>
2020-08-06 09:21:40 -0400

    home start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+import Home from './pages/home'
+import Professional from './pages/professional'
+import Personal from './pages/personal'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/personal">
+            <Personal />Hiiii
+          </Route>
+          <Route path="/professional">
+            <Professional />
+          </Route>
+        </Switch>
+    </Router>
+  )
 }
+
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+        <div class="home">
+            <div class="home-personal">
+                <h1>Raul</h1>
+                <hr/>
+                <p>personal</p>
+            </div>       
+            <div class="home-professional">
+                <h1>Dutta</h1>
+                <hr/>
+                <p>professional</p>
+            </div>     
+        </div>
+
+    </>
+  )
+}
+
+export default Home
