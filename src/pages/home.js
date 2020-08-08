f0e581ebd2f3c322ca4bc73d9908e7a9907f0f9f
duttar18 <duttaraul@gmail.com>
2020-08-07 23:34:12 -0400

    color scheme and full transition

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,12 +7,12 @@ function Home() {
   return (
     <div className="home">
       <a href="/personal" className="home-personal">
-        <h1>Raul</h1>
+        <div className="name">Raul</div>
         <hr/>
         <p>personal</p>
       </a>
       <a href="/professional" className="home-professional">
-        <h1>Dutta</h1>
+        <div className="name">Dutta</div>
         <hr/>
         <p>professional</p>
       </a>
--- src/pages/professional.js
+++ src/pages/professional.js
@@ -1,15 +1,78 @@
+import './personal.css';
 import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
-function Contact() {
-  return (
-    <>
-      <h1>Contact</h1>
-      <p>
-        Aliquam iaculis a nisi sed ornare. Sed venenatis tellus vel consequat
-        congue. In bibendum vestibulum orci et feugiat.
-      </p>
-    </>
-  )
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    x: "-100vw",
+    scale: 0.8
+  },
+  in: {
+    opacity: 1,
+    x: 0,
+    scale: 1,
+    duration: 1,
+  },
+  out: {
+    opacity: 0,
+    x: "100vw",
+    scale: 1.2,
+    duration: 1
+  }
+};
+
+const pageTransition = {
+  ease: "easeOut",
+  type: 'tween',
+  duration: 0.5
+};
+const pageStyle = {
+  position: "absolute"
+};
+
+
+
+const homeVariants = {
+  initial: {opacity: 1},
+  in: { opacity: 1 },
+  out: { opacity: 1 },
 }
 
-export default Contact
+const leftVariant = {
+  initial: {width: '50%' },
+  in: { width: '5%' },
+  out: { width: '50%' },
+}
+const rightVariant = {
+  initial: {width: '50%' },
+  in: { width: '95%' },
+  out: { width: '50%' },
+}
+
+
+function Professional() {
+  return (
+      
+      <motion.div
+        style={pageStyle}
+        initial="initial"
+        animate="in"
+        exit="out"
+        variants={homeVariants}
+        transition={pageTransition}
+        className="home"
+      >
+      <motion.a transition={pageTransition} variants={leftVariant} href="home" className="home-personal">
+        <img src={require("../images/collapse-arrow.png")} className="left-arrow"/>
+      </motion.a>
+      <motion.a  transition={pageTransition} variants={rightVariant} className="home-professional">
+        hiiiii This is my professional
+      </motion.a>
+      </motion.div>
+  );
+}
+
+
+
+export default Professional
